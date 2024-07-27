f285d4ebee6884728aa3a94778eb39a6ee326903
IDFCSGFY <idfcsgfy@gmail.com>
2024-07-28 06:16:13 +0700

    hw12 done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author JL = new Author("Джек", "Лондон");
+        Book book1 = new Book("Мартин Иден", JL, 1909);
+        Author EH = new Author("Эрнест", "Хемингуэй");
+        Book book2 = new Book("Старик и море", EH, 1952);
+
+        System.out.println("book1.getName() = " + book1.getName());
+        System.out.println("book1.getAuthor().getName() = " + book1.getAuthor().getName());
+        System.out.println("book1.getAuthor().getLastname() = " + book1.getAuthor().getLastname());
+        System.out.println("book1.getPublishingYear() = " + book1.getPublishingYear());
+
+        book1.setPublishingYear(2024);
+
+        System.out.println("book1.getPublishingYear() = " + book1.getPublishingYear());
     }
 }
