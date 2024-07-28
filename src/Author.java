6c0c1a8d78a4eaf138b682a8d3d98a04a2d053c2
IDFCSGFY <idfcsgfy@gmail.com>
2024-07-28 14:20:44 +0700

    hw13 done

--- src/Author.java
+++ src/Author.java
@@ -14,4 +14,26 @@ public class Author {
     public String getLastname() {
         return this.lastname;
     }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (other == null || getClass() != other.getClass()) {
+            return false;
+        }
+        Author o = (Author) other;
+        return name.equals(o.name) && lastname.equals(o.lastname);
+    }
+
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(name, lastname);
+    }
+
+    @Override
+    public String toString() {
+        return String.join(" ", name, lastname);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
         Author EH = new Author("Эрнест", "Хемингуэй");
         Book book2 = new Book("Старик и море", EH, 1952);
 
+         /*
+
         System.out.println("book1.getName() = " + book1.getName());
         System.out.println("book1.getAuthor().getName() = " + book1.getAuthor().getName());
         System.out.println("book1.getAuthor().getLastname() = " + book1.getAuthor().getLastname());
@@ -13,5 +15,16 @@ public class Main {
         book1.setPublishingYear(2024);
 
         System.out.println("book1.getPublishingYear() = " + book1.getPublishingYear());
+
+          */
+
+        //создание копии для проверки методов
+        Author EH2 = new Author("Эрнест", "Хемингуэй");
+        Book book3 = new Book("Старик и море", EH2, 2024);
+        //проверка .toString()
+        System.out.println("book2 = " + book2);
+        System.out.println("book3 = " + book3);
+        //проверка .equals()
+        System.out.println("book2.equals(book3) = " + book2.equals(book3));
     }
 }
